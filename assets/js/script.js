0a1909c25c1b20b8fa52f55ecc7ea6f3c2e4683b
Ashley Hodge <ashhodge@comcast.net>
2022-01-20 12:44:51 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,16 +173,18 @@ $(".card .list-group").sortable({
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-        
+        $(this).addClass("dropover")
+        $(".bottom-trash").addClass("bottom-trash-drag")
     },
     deactivate: function(event) {
-        
+        $(this).removeClass("dropover")
+        $(".bottom-trash").removeClass("bottom-trash-drag")
     },
     over: function(event) {
-        
+        $(event.target).addClass("dropover-active")
     },
     out: function(event) {
-        
+        $(event.target).removeClass("dropover-active")
     },
     update: function(event) {
         var tempArr = [];
@@ -221,12 +223,13 @@ $("#trash").droppable({
     tolerance: "touch",
     drop: function(event,ui) {
     ui.draggable.remove()    
+        $(".bottom-trash").removeClass("bottom-trash-active")
     },
     over: function(event,ui) {
-        console.log("over");
+       $(".bottom-trash").addClass("bottom-trash-active")
     },
     out: function(event,ui) {
-        console.log("out");
+        $(".bottom-trash").removeClass("bottom-trash-active")
     }
 });
 $("#modalDueDate").datepicker({
@@ -249,7 +252,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -281,3 +284,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+    $(".card .list-group-item").each(function(index,el) {
+        auditTask(el)
+    });
+}, 1800000);
