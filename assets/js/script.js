bed31dc85e86fd532547ae1d354bc80e1e1cbda8
Ashley Hodge <ashhodge@comcast.net>
2022-01-19 19:31:11 -0500

    add drag and drop features

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -135,7 +134,68 @@ $(".list-group").on("blur", "input[type='text']", function() {
     $(this).replaceWith(taskSpan);
 })
 
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+        
+    },
+    deactivate: function(event) {
+        
+    },
+    over: function(event) {
+        
+    },
+    out: function(event) {
+        
+    },
+    update: function(event) {
+        var tempArr = [];
+        
+        // loop over current set of children in sortable list
+        $(this).children().each(function() {
+            
+            var text = $(this)
+                .find("p")
+                .text()
+                .trim();
+            var date = $(this)
+                .find("span")
+                .text()
+                .trim();
+            // add task data to the temp array as an objuct
+            tempArr.push({
+                text: text,
+                date: date
+            });
+       
+        });
+         // trim down list's ID to match object property
+        var arrName = $(this)
+            .attr("id")
+            .replace("list-", "");
 
+        // update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+    }
+});
+
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event,ui) {
+    ui.draggable.remove()    
+    },
+    over: function(event,ui) {
+        console.log("over");
+    },
+    out: function(event,ui) {
+        console.log("out");
+    }
+});
 
 
 // modal was triggered
